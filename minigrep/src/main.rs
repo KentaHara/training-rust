131353d1ed8f3e5bf7a0cd00018da64f88951103
KentaHara <blanc.et.noir.rinc@gmail.com>
2020-01-11 11:20:44 +0900

    [modify] `eprintln!`

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,19 +7,19 @@ fn main() {
     // Accepting Command Line Arguments
     //
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // println!("{:?}", args);
 
     // ロジックを `parse_config` に譲渡
     // let query = &args[1];
     // let filename = &args[2];
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
     // ------------------------
 
@@ -31,7 +31,7 @@ fn main() {
     // println!("With text:\n{}", contents);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
